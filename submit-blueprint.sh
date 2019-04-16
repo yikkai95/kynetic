3e253702f0c642183baf53a3a81d9ab68b2995b2
Ng Yik Kai <yikkai95@gmail.com>
2019-04-16 13:04:28 +0800

    [IMP] Use user argument on submiting blueprint

--- submit-blueprint.sh
+++ submit-blueprint.sh
@@ -1,5 +1,5 @@
 TYPE=$1
 source ./.env
-curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/blueprints/multi-container-kylin --data-binary "@examples/blueprints/$TYPE.json"
+curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/blueprints/$TYPE --data-binary "@examples/blueprints/$TYPE.json"
 curl --user admin:admin -H 'X-Requested-By:admin' -X PUT $AMBARI_HOST/api/v1/stacks/HDP/versions/$HDP_VERSION/operating_systems/$OS/repositories/HDP-${HDP_VERSION} -d '{"Repositories":{"base_url":"'$BASE_URL'", "verify_base_url":true}}'
 curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/clusters/dev --data-binary "@examples/hostgroups/$TYPE.json"
